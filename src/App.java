42dc86293571b87dd02e7b69a44edc17af97b92e
lopezatn <agustinlpzb@gmail.com>
2025-03-18 14:19:10 +0100

    overloaded function seconds exercise

--- src/App.java
+++ src/App.java
@@ -1,10 +1,13 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("inches to cm = " + convertToCentimeters(2));
-        System.out.println("feet + inches to cm = " + convertToCentimeters(2, 4));
+
+        System.out.println(getDurationString(9200));
+        System.out.println(getDurationString(90, 240));
+
     }
 
+    // overloaded methods:
     public static double convertToCentimeters(int inches) {
         return inches * 2.54;
     }
@@ -13,4 +16,35 @@ public class App {
         return convertToCentimeters((int) feet*12+inches);
     }
 
+    public static String getDurationString (int seconds) {
+        int minutes = 0;
+        int hours = 0;
+
+        while (seconds >= 3600) {
+            hours++;
+            seconds -= 3600;
+        } 
+        while (seconds >= 60) {
+            minutes++;
+            seconds -= 60;   
+        }
+        return String.format("%02dh %02dm %02ds", hours, minutes, seconds);
+    }
+
+    public static String getDurationString (int minutes, int seconds) {
+        int hours = 0;
+
+        while (seconds >= 60) {
+            minutes++;
+            seconds -= 60;
+        }
+
+        while (minutes >= 60) {
+            hours++;
+            minutes -= 60;
+        }
+
+        return String.format("%02dh %02dm %02ds", hours, minutes, seconds);
+    }
+    // end of overloaded methods
 }
