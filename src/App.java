0bb8716b1a2dcf016767c6b8f9182d269a1fe285
lopezatn <agustinlpzb@gmail.com>
2025-03-18 09:36:46 +0100

    overloading methods challenge

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        System.out.println("inches to cm = " + convertToCentimeters(2));
+        System.out.println("feet + inches to cm = " + convertToCentimeters(2, 4));
     }
+
+    public static double convertToCentimeters(int inches) {
+        double inchesToCms = inches * 2.54;
+
+        return inchesToCms;
+    }
+
+    public static double convertToCentimeters(int feet, int inches) {
+        int totalInches = (int) feet * 12 + inches;
+
+        double resultOfInches = convertToCentimeters(totalInches);
+        return resultOfInches;
+    }
+
 }
