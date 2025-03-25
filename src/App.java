200cfb01e260cc71de308a993830bcff15e0ada7
lopezatn <agustinlpzb@gmail.com>
2025-03-25 11:51:16 +0100

    while loop exercises

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@ public class App {
 
     public static void main(String[] args) {
 
-        printEvenNumbers();
+        int number = sumDigits(7);
+        System.out.println(number);
 
     }
 
@@ -224,6 +225,21 @@ public class App {
         //     }
         //     number++;
         // }
+    }
+
+    public static int sumDigits (int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int sum = 0;
+        int lastDigit = 0;
+        while (number > 0) {
+            lastDigit = number % 10;
+            sum += lastDigit;
+            number = number / 10;
+        }
+        return sum;
+
     }
 
     // end of while loop
